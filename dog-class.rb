1edbb632b597bc83307655c33acc21c753068f1c
jcheng98 <jessiecheng666@aol.com>
2014-06-23 13:42:16 -0400

    dog class

---
+++ dog-class.rb
@@ -0,0 +1,37 @@
+class Dog
+  def initialize(name, breed, leg_count)
+    @name = name
+    @breed = breed
+    @leg_count = leg_count
+  end
+
+  def bark
+    puts "BARK! WOOF!"
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name=(name)
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed=(breed)
+  end
+
+  def leg_count
+    @leg_count
+  end
+
+  def leg_count=(leg_count)
+    @leg_count=(leg_count)
+  end
+end
+
+fido = Dog.new
